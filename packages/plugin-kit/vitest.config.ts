9765505f872da7ebc18ca101e6b127adbc363570
Manas Madan <manasmadan08@gmail.com>
2026-06-22 09:20:59 +0000

    chore(plugin-kit): vitest config

---
+++ packages/plugin-kit/vitest.config.ts
@@ -0,0 +1,15 @@
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  test: {
+    include: ['test/**/*.test.ts'],
+    testTimeout: 30_000,
+    hookTimeout: 120_000,
+  },
+})
